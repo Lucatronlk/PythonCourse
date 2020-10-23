1581e274d19c8f5eef9f48c38d83a97cbad69726
Lucatronlk <alex.luka@hotmail.com>
2020-10-23 06:25:59 +0300

    Course1,2 and 3

---
+++ Course1/homework - Course1_Luca-Postescu_Alexandru.py
@@ -0,0 +1,190 @@
+
+#  Bool/comparison operators:
+#  1. write 3 comparisons which are True
+#  2. write 3 comparisons which are False
+#  3. write a comparison in a function and call it 3 times with different parameters
+#  4. check the functions output using if
+
+comp_1 = 10
+comp_2 = 20
+comp_3 = 30
+comp_4 = 40
+comp_5 = 50
+
+print(comp_1 < comp_2)
+print(comp_2 > comp_1)
+print(comp_3 > comp_2)
+
+print(comp_3 < comp_1)
+print(comp_2 < comp_1)
+print(comp_4 > comp_5)
+
+comp = comp_1
+#comp = comp_2
+#comp = comp_3
+
+def comp_func(comp):
+ if comp > 60: 
+  print('Ceva') 
+ elif comp < 60:
+  print('AsaCeva')
+ else:
+  print('Altceva')
+  
+#  Operations with int/float:
+#  1. Write a function which makes the sum of 3 ints
+#  2. Write a function which computes 2 power to the specified variable 
+#  3. Write a function which makes the sum of 4 floats
+#  4. Write a function which makes diff of 2 floats & multiplies it with a int 
+#  5. Write a function which computes how many apples go into a specified number of boxes
+#  6. Check the functions output using if 
+
+#1
+def sum_int(a, b, c):
+
+     sum = a + b + c
+
+     if a != b != c:
+         sum = a + b +c
+         return sum 
+     if a == b == c:
+         sum = sum * 3
+         return sum 
+     
+print(sum_int(1, 2, 3))
+print(sum_int(3, 3, 3))
+
+#2
+def pow_int(a, b):
+    pow = a ** b
+    return pow
+
+print(pow_int(3, 3))
+
+#3
+
+def sum_float(a, b, c, d):
+
+     sum = a + b + c + d
+     return sum
+
+print (sum_float(12.1, 13.4, 2.3, 4.8))
+    
+#4 Write a function which makes diff of 2 floats & multiplies it with a int 
+
+f = 8.9
+l = 21.8
+i = 12
+
+diff = f - l
+print (diff)
+
+multi = diff * i
+
+print (multi)
+
+#5 Write a function which computes how many apples go into a specified number of boxes
+
+def number_apples(cos, mar_rosu, mar_verde):
+    return (mar_rosu + mar_verde) - cos + 1
+
+if __name__ == '__main__': 
+       cos = 3
+       mar_rosu = 6
+       mar_verde = 3
+
+print("Nr Total de Mere tinut este =", 
+       number_apples(cos, mar_rosu, mar_verde))
+    
+
+
+
+#  Operations with strings:
+#  0. Select the 5th letter of 3 Strings 
+#  1. Make 3 substrings with only the first 6 chars of a String
+#  2. Make 3 substrings with only the last 3 chars of a String
+#  3. Make 3 substrings without the first 4 chars of a String
+#  4. Put together 4 strings 
+#  5. Show the same string 15 times 
+#  6. Define functions for all the exercises above
+#  7. Check the output of the functions with if
+
+#0
+print("uncuvant"[5])
+print("altcuvant"[5])
+print("cuvantulcuvantului"[5])
+
+#1
+string_1 = "exercises"
+string_2 = "functions"
+string_3 = "substrings"
+string_4 = "letter"
+
+print(string_1[0:5])
+print(string_2[0:5])
+print(string_3[0:5])
+
+#2
+
+print(string_1[-3:])
+print(string_2[-3:])
+print(string_3[-3:])
+
+#3
+
+print(string_1[4:])
+print(string_2[4:])
+print(string_3[4:])
+
+
+#4
+
+print (string_1 + string_2 + string_3 + string_4)
+
+#5
+
+print (string_1 * 15)
+
+#  Functions and ifs:
+#  1. Calculate tax for a apartments based on space
+#     a. 20mp => 100ron
+#     b. 50mp => 300ron
+#     c. 100mp => 500ron
+#     d. 200mp => 2000ron
+#  2. Calculate tax on a machine based on its engine
+#  3. Check the functions output using if 
+
+#1 
+
+def tax_appart(tax):
+    
+    if space == 20:
+      return "Taxa este de 100 ron" 
+    elif space == 50:
+        return "Taxa este de 300 ron"
+    elif space == 100:
+        return "Taxa este de 500 ron"
+    elif space == 200:
+        return "Taxa este de 2000 ron"
+
+space = 20
+print(tax_appart(space))
+
+#2  
+
+def tax_car(tax):
+    if engine < 1.9:
+        return " Taxa motor este de 50 lei"
+    elif engine >= 2.0:
+        return "Taxa motor este de 90 lei"
+    elif engine >= 3.0:
+        return "Taxa motor este de 150 lei"
+    elif engine >= 4.0:
+        return "Taxa de motor este 300 lei"
+    elif engine > 5.0:
+        return "Taxa de motor este 500 lei"
+
+
+engine = 2.5
+
+print(tax_car(engine))
---
+++ Course2/Homework2.py
@@ -0,0 +1,114 @@
+from math import gcd
+import operator
+
+#1. Generate & print the list of the first 500 odd numbers
+
+# list=[]
+# i = 1
+# while i <= 500:
+#     list.append(i)
+#     i += 1
+#     n = 2 * i - 1
+#     print(n)
+#     i = i + 1 
+# print (list)
+ 
+list = []
+i = 0
+while len(list) < 500:
+  i += 1
+  if i % 2 == 1:
+    list.append(i)
+print(list)
+print(len(list))
+
+# 2.Generate & print the first 30 prime numbers
+
+list_odd  = []
+i = 0
+while i <= 30:
+    list_odd.append(i)
+    i += 1
+
+start = 0
+end = 30
+for i in range(start, end):
+    if i > 1:
+        for j in range(2, i):
+            if (i % j) == 0:
+                break
+
+        else:
+            print(i)
+              
+
+# 3. Create a game in which the user must guess an animal. He has 20 tries
+
+animal = "Leu"
+print("Te rog sa ghicesti animalul")
+user_animal = input()
+
+turns = 20 
+
+while turns > 0:
+    if user_animal == animal:
+        print("Ai ghicit Animalul")
+        break   
+    else:
+     print("Nu ai ghicit")
+     print ("Incearca din nou")
+     user_animal = input()
+     turns -= 1     
+
+if turns == 0:
+     print ("Game Over")  
+
+# 4. Create a game in which the user must guess the type of a car. The program stops only when the user guesses
+
+car = "Volvo"
+print ("Te rog sa ghicesti marca de masina")
+user_car=input()
+
+while True:
+    if car == user_car:
+        print("Raspuns Corect")
+        break
+    else:
+        print("Raspuns gresit, Te rog mai incearca")
+        user_car=input()
+    
+# 5. Create a function which takes 2 ints and returns the greatest common divisor & the lowest common multiple. Make 5 tests for the function
+
+print("Primul numar")
+user_int1=input()
+print("Al doilea numar")
+user_int2=input()
+
+def lcm_gcd (user_int1, user_int2):
+    def gcd(user_int1, user_int2):
+        while user_int2 > 0:
+            user_int1, user_int2 = user_int2, user_int1 % user_int2
+            return user_int1
+
+def lcm(user_int1, user_int2):
+        return user_int1 * user_int2 / gcd(user_int1, user_int2)
+
+print(gcd(user_int1, user_int2))
+print(lcm(user_int1, user_int2))
+
+
+# 6. Make a dictionary with 10 key/values. The keys represent the name of a product & the value the price of the product. 
+# Create a function which returns the name of the product with the lowest price above 100.
+
+
+dict={'monitor': 2700, 'laptop': 5000, 'mouse': 100, 'tastatura': 200, 'cablu HDMI': 10, 'cooler': 40, 'boxe mici': 80, 'sistem audio 5.1': 350, 'stick usb': 90, 'mouse pad': 70}
+
+lowest_price = 10000000000
+lowest_product = ''
+for produs,pret in dict.items():
+  if lowest_price > pret and pret >= 100:
+    lowest_price = pret
+    lowest_product = produs
+print(lowest_product)
+
+    
