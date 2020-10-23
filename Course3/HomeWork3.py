ac6e294ec6faea2234872d6ac5527888d1c45ddd
Lucatronlk <alex.luka@hotmail.com>
2020-10-23 13:48:42 +0300

    homework3_p1

---
+++ Course3/HomeWork3.py
@@ -0,0 +1,24 @@
+# 1. De cate ori apare numarul 8 in lista?
+
+list = [1, 10, 9, 8, 8, 7, 8, 11, 2, 7, 89, 8, 90,24, 8, 5] 
+
+my_list = {i:list.count(i) for i in list}
+print(my_list)
+
+#Generati primele 50 de numere in ordine descrescatoare
+
+list_2 = []
+i = 0
+while i < 50:
+    i += 1
+    list_2.append(i)
+    
+list_2.sort(reverse=True)
+    
+print(list_2)
+
+#3 Creati o noua lista care sa fie ca cea de mai sus dar fara litera a list = ["a", "b", "c", "a", "d", "e", "b", "a"] 
+
+list_3=[]
+
+
---
+++ Course3/tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+list_2 = []
+i = 0
+while i < 50:
+    i += 1
+    list_2.append(i)
+    
+list_2.sort(reverse=True)
+    
+print(list_2)
