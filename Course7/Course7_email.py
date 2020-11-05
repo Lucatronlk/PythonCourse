f4c580c7e288a51b0966b1931491e19bf502a3fa
Lucatronlk <alex.luka@hotmail.com>
2020-11-05 07:49:04 +0200

    email

--- Course7/Course7_email.py
+++ Course7/Course7_email.py
@@ -1,32 +1,48 @@
 class EmailUser:
- 
- _email_list = [] # lista privata
- _max_number_of_emails = 10
- 
- def __init__(self, username, full_name):
+  _email_list = []
+  __max_number_of_emails = 10
+
+  def __init__(self, username, full_name):
     self._username = username
     self._full_name = full_name
     try:
-        open(self, username)
-    f = open(self._username, 'w')
-    f.write('username:' +self._username + '\n')
-    f.write('full_name:' +self._full_name + '\n')
+      f = open(self._username)
+      f.close()
+    except FileNotFoundError:
+      self.__save()
 
- def get_username(self):
+  def get_username(self):
     return self._username
- 
- def get_full_name(self):
-     return self._full_name   
 
- def get_emails(self):  # metoda ca sa access lista 
-     return self._email_list
-
- def _delete_old_emails(self):
-     if len(self._email_list) > self._max_number_of_emails:
-        self._email_list = self._email_list[1:]
-
- def add_email(self, subject, message):
-    self._email_list.append({'subject': subject, 'message': message})
+  def add_email(self, subject, message):
+    self._email_list.append({"subject": subject, "message": message})
+    self.__save_email(subject, message)
     self._delete_old_emails()
 
+  def get_emails(self):
+    f = open(self._username)
+    return f.read()
+    return self._email_list
 
+  def _delete_old_emails(self):
+    if len(self._email_list) > self.__max_number_of_emails:
+      self._email_list = self._email_list[1:]
+
+  def __save(self):
+    f = open(self._username, 'w')
+    f.write('username: ' + self._username + '\n')
+    f.write('full_name: ' + self._full_name + '\n')
+
+  def __save_email(self, subject, message):
+    f = open(self._username, 'a')
+    f.write('subject: ' + subject + ' , message: ' + message + '\n')
+    f.close()
+
+
+class PremiumEmailUser(EmailUser):
+  def __init__(self, username, full_name):
+    self._username = username
+    self._full_name = full_name
+
+  def _delete_old_emails(self):
+    return
--- Course7/premium_email.py
+++ Course7/premium_email.py
@@ -1,25 +1,11 @@
 from email import EmailUser
 
-
-class PremiumEmailUser(EmailUser):
-    def __init__(self, username, full_name):
-        self._username = username
-        self._full_name = full_name
-
-    def _delete_old_emails(self):
-        return
-
-
-user1 = PremiumEmailUser("alexandru.luca@gmail.com", "Alexandru Luca")
-
+user1 = EmailUser("alexandru.luca@gmail.com", "Luca Alexandru")
 print(user1.get_username())
+print(user1.get_emails())
 
-# print(user1.get_full_name)
-# user1.add_email("subj1", "hello")
-# print(user1.get_emails())
-
-i = 0
-while i < 12:
-    user1.add_email('s' + str(i), 'm' + str(i))
-    i += 1
-    print(user1.get_emails())
+i = 3
+while i < 6:
+  user1.add_email('s' + str(i), 'm ' + str(i))
+  i += 1
+  print(user1.get_emails())
