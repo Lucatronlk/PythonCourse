8655f9a9ed7a7c33eb7dc77763ab3f61cd7b4d2b
Lucatronlk <alex.luka@hotmail.com>
2021-01-06 18:42:42 +0200

    flower shop app

---
+++ Course19/homework-holiday_homework/flower_shop.py
@@ -0,0 +1,72 @@
+# Create a flower shop application which deals in flower objects 
+# and use those flower objects in a bouquet object which can then be sold. 
+# Keep track of the number of objects and when you may need to order more (keep them in a file) 
+# Allow the user to give commands through the terminal (use sys.argv to read the commands)
+# Make unit tests
+
+import sys
+import abc
+
+class Flower(abc.ABC):
+
+    @abc.abstractmethod
+
+    def __init__(self, colour, smell, petals):
+        self.colour = colour
+        self.smell = smell
+        self.petals = petals
+
+
+    # def __init__(self, rose, tulip, lily, orchid):
+    #     self.rose = rose
+    #     self.tulip = tulip
+    #     self.lily = lily
+    #     self.orchid = orchid
+
+
+class Rose(Flower):
+
+    def __init__(self, colour,  petals):
+
+        super().__init__(colour, 'relaxing', petals)
+
+
+class Tulip(Flower):
+
+    def __init__(self, colour, petals):
+        super().__init__(colour, 'honey', petals)
+
+
+class Orchid(Flower):
+    def __init__(self, colour, petals):
+        super().__init__(colour, 'cinnamon', petals)
+
+class Bouquet:
+    def __init__(self, flowers):
+
+        self.flowers = flowers
+
+    def get_smell(self):
+         smells = {}
+         for one_flower in self.flowers:
+             if one_flower.smell in smells:
+               smells[one_flower.smell] += 1
+             else:
+                 smells[one_flower.smell] = 1
+         return smells
+
+
+
+
+class FlowerShop:
+    def place_order(self, flowers):
+        bouquet = Bouquet(flowers)
+        return bouquet
+
+
+shop = FlowerShop()
+order = shop.place_order([Tulip('red', 6), Orchid('white', 3), Orchid('white', 4)])
+print(order.get_smell())
+
+
+
---
+++ Course19/homework-holiday_homework/recipes.py
@@ -0,0 +1,74 @@
+# Create a recipe class with ingredients and a put them in a recipe manager program 
+# that organizes them into categories like deserts, main courses 
+# or by ingredients like chicken, beef, soups, pies etc.
+
+
+import sys
+
+
+# Recipe is a representation of a cooking recipe
+class Recipe:
+    def __init__(self):
+        self.ingredients = []
+        self.instructions = []
+        self.name = ""
+
+
+# Save/Store it into dictionary, Test it
+def create():
+    recipe = Recipe()
+    recipe_dict = {}
+
+    print("Create a recipe!")
+    recipe.name = input("Recipe Name: ")
+
+    while True:
+        ingredients = input("Ingredients: ")
+        if ingredients == 'exit':
+            break
+        recipe.ingredients.append(ingredients)
+
+    while True:
+        instructions = input("Instructions: ")
+        if instructions == 'exit':
+            break
+        recipe.instructions.append(instructions)
+
+    recipe_dict[recipe.name] = recipe
+
+
+def listrecipe():
+    print("listrecipe")
+
+
+def update():
+    print("update")
+
+
+def delete():
+    print("delete")
+
+
+def main():
+    # check if user has entered enough arguments
+    if len(sys.argv) != 2:
+        print("Usage: recipe <Action> [args]")
+        sys.exit(1)
+
+    # argv stores arguments to your program
+    action = sys.argv[1]
+
+    if "create" == action:
+        create()
+    elif "list" == action:
+        listrecipe()
+    elif "update" == action:
+        update()
+    elif "delete" == action:
+        delete()
+    else:
+        print("command not found")
+        sys.exit(1)
+
+
+main()
